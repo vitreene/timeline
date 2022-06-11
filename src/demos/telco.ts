ab42e3665e23d91987a8eb7de584744914304f62
vitreene <herve@vitreene.com>
2022-06-11 15:36:57 +0200

    sync slider progression

--- src/demos/telco.ts
+++ src/demos/telco.ts
@@ -1,6 +1,7 @@
 import { Clock } from '../timeline';
 
-import { END_SEQUENCE } from '../common/constants';
+import { END_SEQUENCE, PLAY } from '../common/constants';
+import { CbStatus } from 'src/clock';
 
 //SLIDER////////////
 export function createTelco() {
@@ -14,25 +15,43 @@ export function createTelco() {
 	slider.addEventListener('mouseup', () => {
 		slider.removeEventListener('mousemove', mousemove);
 	});
-	function mousemove(e: Event): void {
-		const el = e.target as HTMLInputElement;
-		const p = (Number(el.value) * END_SEQUENCE) / 100 - 100;
+
+	function mousemove(): void {
+		const p = (Number(slider.value) * END_SEQUENCE) / 100 - 100;
 		const progression = p > 0 ? p : 0;
-		progress.textContent = String(progression);
+		progress.textContent = Math.round(Number(slider.value)) + '%';
+
 		Clock.seek(progression);
 	}
+
+	let toggle = true;
+	function togglePlay(): void {
+		if (toggle) {
+			Clock.pause();
+			playButton.innerText = 'play';
+			toggle = false;
+		} else {
+			Clock.play();
+			playButton.innerText = 'pause';
+			toggle = true;
+		}
+	}
 	const playButton = document.createElement('button');
-	playButton.innerText = 'play';
-	playButton.addEventListener('click', () => Clock.play());
-	const pauseButton = document.createElement('button');
-	pauseButton.innerText = 'pause';
-	pauseButton.addEventListener('click', () => Clock.pause());
+	playButton.innerText = 'pause';
+	playButton.addEventListener('click', togglePlay);
 
 	const progress = document.createElement('span');
 
 	telco.appendChild(playButton);
 	telco.appendChild(slider);
-	telco.appendChild(pauseButton);
 	telco.appendChild(progress);
 	document.body.appendChild(telco);
+
+	Clock.onTick((status: CbStatus) => {
+		if (status.action === PLAY) {
+			const progression = (status.currentTime * 100) / END_SEQUENCE;
+			slider.value = String(progression);
+			progress.textContent = Math.round(progression) + '%';
+		}
+	});
 }
