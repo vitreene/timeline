8cae2d20c3a8b5647d7af02bec46a5e2c4fd54b8
vitreene <herve@vitreene.com>
2024-04-01 22:05:57 +0200

    fix import palyer

--- editor/src/lib/player/build-play.ts
+++ editor/src/lib/player/build-play.ts
@@ -1,5 +1,7 @@
 import type { SceneComp } from '$lib/server/db';
+import type { MapEvent } from '@vitreene/player';
+import { Controller } from '@vitreene/player';
 
 export function buildPlay(scene: SceneComp) {
-	const events = new Map();
+	const events: MapEvent = new Map();
 }
