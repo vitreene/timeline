fde4a5c23ad35ac86b6e6a77edaa1ccc2891b635
vitreene <herve@vitreene.com>
2024-04-02 22:13:13 +0200

    wip build-play

--- editor/src/lib/player/build-play.ts
+++ editor/src/lib/player/build-play.ts
@@ -4,4 +4,31 @@ import { Controller } from '@vitreene/player';
 
 export function buildPlay(scene: SceneComp) {
 	const events: MapEvent = new Map();
+
+	const mediasEvents = new Map(scene.medias.flatMap((m) => m.events).map((e) => [e.id, e]));
+
+	const sceneEvents = new Map();
+	for (const capsule of scene.capsules) {
+		for (const element of capsule.elements) {
+			// il va y avoir des duplis : non
+			element.events.forEach((e) => sceneEvents.set(e.action, e.name));
+		}
+	}
+
+	const actionsByMedia = {};
 }
+
+//TODO
+/* 
+	construire la liste des actions avec l'indicateur de temps
+	e.name = ref TimeEvent -> start
+	e.action -> name
+	Map([3000, { name: 'action03', data: {} }])
+	
+	*/
+/* 
+	pour chaque media dans chaque capsule :
+	- identitfier le textime de chaque action;
+	faire un tableau  [TextTime[e.name].start : { name: e.action } ] // [ ]
+	
+	*/
--- player/lib/main.ts
+++ player/lib/main.ts
@@ -1,5 +1,7 @@
 import { Controller } from './sceneline/controller';
 export * from './sceneline/constants';
+
+// cet export n'est plus défini au build !
 export * from '../types';
 
 export { Controller };
